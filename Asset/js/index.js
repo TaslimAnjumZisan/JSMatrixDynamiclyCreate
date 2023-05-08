4466ee92acab63b6e12c819d957e57928c84ed12
TasnimAnjum <114102739+TasnimAnjum@users.noreply.github.com>
2023-05-08 16:36:16 +0600

    Matrix done

---
+++ Asset/js/index.js
@@ -0,0 +1,24 @@
+function createTable(){
+    let row,col;
+    document.write(document.getElementById('nav').innerHTML);
+    document.write("<table border='1px' width='100' height='100'>");
+    for(row=0;row<5;row++)
+    {   
+        document.write("<tr>");
+        for(col=0;col<5;col++)
+        {
+            console.log(col.toString())
+            let id = row.toString()+col.toString();
+            console.log(id);
+            document.write("<th id="+ id +">" +Math.floor(Math.random()*50)+ "</th>");
+        }
+        document.write("</tr>");
+    }
+}
+
+
+function change(){
+    console.log(document.querySelectorAll('tr'));
+    console.log(document.querySelectorAll('th'));
+    // var col_arr []='tr';
+}
