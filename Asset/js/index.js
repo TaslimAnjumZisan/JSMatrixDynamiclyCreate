5e91918fc8df954674df3b52a69f9852ea708e08
TasnimAnjum <zishantasnim@gmail.com>
2023-05-09 03:47:30 +0600

    Change value done

--- Asset/js/index.js
+++ Asset/js/index.js
@@ -22,7 +22,30 @@ function createTable(){
 
 
 function change(){
-    console.log(document.querySelectorAll('tr'));
-    console.log(document.querySelectorAll('th'));
+    //console.log(document.querySelectorAll('tr'));
+    //console.log(document.querySelectorAll('th'));
     // var col_arr []='tr';
+
+    //var table = $("#table")[0];
+//var cell = table.rows[1].cells[1]; // This is a DOM "TD" element
+//var $cell = $(cell); // Now it's a jQuery object.
+//console.log($cell);
+
+//et id = row.toString()+col.toString();
+            //console.log(id);
+
+    var ro=document.getElementById("row").value-1;
+    var co=document.getElementById("column").value-1;
+    var va=document.getElementById("value").value;
+    console.log(va);
+    document.getElementById(ro+""+co).innerHTML=va;
+
+}
+
+function sum(){
+    // console.log(document.querySelectorAll('tr'));
+    // console.log(document.querySelectorAll('th'));
+        var table = document.getElementById("nav");
+        var cell = table.rows[0].cells[0]+table.rows[0].cells[1]+table.rows[0].cells[2]+table.rows[0].cells[3]+table.rows[0].cells[4];
+        console.log(cell);
 }
